62a4c1fe77f47f95fc9ede5436cef501b82e3d02
Sarthak Arora <sarthak.arora@gmail.com>
2020-02-19 11:29:01 -0500

    added 2nd test case. Updated run.sh

--- src/border_analysis.py
+++ src/border_analysis.py
@@ -1,10 +1,9 @@
 import csv
-
+import argparse
 csvpath = '../input/Border_Crossing_Entry_Data.csv' # small file 6 records
-csvpath2 = '../input/Border_Crossing_Entry_Data2.csv' # large file 300k records
+# csvpath2 = '../input/Border_Crossing_Entry_Data2.csv' # large file 300k records
 
 data = {}
-data2 = {}
 
 def readfile(csvpath, dictionary):
     ## Function to read csv from given path and store data columnwise in a dictionary.
@@ -91,93 +90,102 @@ def link_twocols(groupby, x):
 
     return temp
 
-# data = readfile(csvpath, data) # reading small file
+if __name__ == '__main__':
 
-data = readfile(csvpath2, data) # reading large file
+    parser = argparse.ArgumentParser()
+    parser.add_argument("csvpath")
+    parser.add_argument("outputpath")
+    csvpath = parser.parse_args().csvpath
+    outputpath = parser.parse_args().outputpath
 
-keys = ['Port Name','State','Port Code','Border','Date','Measure','Value'] # columns of data stored as keys in dictionary
+    # data = readfile(csvpath, data) # reading small file
 
+    data = readfile(csvpath, data) # reading large file
 
-# for data2 (large file)
-# groupby_date = groupby_column(data2,keys[4]) # grouping on dates. getting data indices
-# groupby_measure = groupby_column(data2,keys[5]) # grouping on measure. getting data indices
-# groupby_border = groupby_column(data2,keys[3]) # grouping on border. getting data indices
-
-# for data (small file)
-groupby_date = groupby_column(data,keys[4])    # grouping on dates. getting data indices
-groupby_measure = groupby_column(data,keys[5]) # grouping on measure. getting data indices
-groupby_border = groupby_column(data,keys[3])  # grouping on border. getting data indices
-
-# keys or columns of report.csv
-reportkeys = ['Border','Date','Measure','Value','Average']
-
-## Forming a dictionary to group and sort data based on date, measure and intersection. Calculating Value of a measure in a month on a particular border.
-f2 = {}
-for key in reportkeys:
-    f2[key] = []
-
-for date in sorted(groupby_date,reverse=True):
-    for measure in sorted(groupby_measure):
-        for border in sorted(groupby_border):
-            # intersection gives us the list of indices that are common for a border,measure and date.
-            intersection = list(set(groupby_measure[measure]) & set(groupby_date[date]) & set(groupby_border[border]))
-#             print(intersection)
-            if len(intersection) != 0:
-#                 res = [int(data[keys[6]][i]) for i in intersection]   # for small file
-                res = [int(data[keys[6]][i]) for i in intersection]   # for large file
-#                 print(border,date,measure,sum(res))
-                f2[reportkeys[0]].append(border)
-                f2[reportkeys[1]].append(date)
-                f2[reportkeys[2]].append(measure)
-                f2[reportkeys[3]].append(sum(res))
-                f2[reportkeys[4]].append(0)
-
-f2 = compute_movavg(f2)
-
-groupby_value = groupby_column(f2, reportkeys[3])   # grouping on values. getting data indices
-groupby_average = groupby_column(f2, reportkeys[4])  # grouping on average. getting data indices
-groupby_date = groupby_column(f2, reportkeys[1])   # grouping on dates. getting data indices
-groupby_measure = groupby_column(f2, reportkeys[2])  # grouping on measure. getting data indices
-groupby_border = groupby_column(f2, reportkeys[0])  # grouping on border. getting data indices
-
-## Sequentually this is the order in which the sorting of the outut file needs to be. Date -> Value -> Measure -> Border
-t1 = link_twocols(groupby_date,3) # linking date and value
-t2 = link_twocols(groupby_value,2) # linking value and measure
-t3 = link_twocols(groupby_measure,0) # linking measure and border
+    keys = ['Port Name','State','Port Code','Border','Date','Measure','Value'] # columns of data stored as keys in dictionary
 
 
-## Writing final output to csv
+    # for data2 (large file)
+    # groupby_date = groupby_column(data2,keys[4]) # grouping on dates. getting data indices
+    # groupby_measure = groupby_column(data2,keys[5]) # grouping on measure. getting data indices
+    # groupby_border = groupby_column(data2,keys[3]) # grouping on border. getting data indices
 
-i = 0
-j = 0
-k = 0
-l = 0
-filename = '../output/report.csv'
-# writing to csv file
-with open(filename, 'w') as csvfile:
-    # creating a csv writer object
-    csvwriter = csv.writer(csvfile)
-    # writing the fields
-    csvwriter.writerow(reportkeys)
-    for date in t1:
-    #     print(date)
-        i = 0
-        while(i!=len(t1[date])):
-            value = t1[date][i]
-            i = i + 1
-    #         print(value)
-            j = 0
-            while(j!=len(t2[value])):
-                measure = t2[value][j]
-                j = j + 1
-    #             print(measure)
-                k = 0
-                while(k!=len(t3[measure])):
-                    border = t3[measure][k]
-                    intersection = list(set(groupby_measure[measure]) & set(groupby_date[date]) & set(groupby_border[border]) & set(groupby_value[value]))
-                    if len(intersection)!=0:
-                        index = intersection[0]
-#                     print(border,date,measure,value,f2[reportkeys[4]][index])
-                        row = [border,date,measure,value,f2[reportkeys[4]][index]]
-                        csvwriter.writerow(row)
-                    break
+    # for data (small file)
+    groupby_date = groupby_column(data,keys[4])    # grouping on dates. getting data indices
+    groupby_measure = groupby_column(data,keys[5]) # grouping on measure. getting data indices
+    groupby_border = groupby_column(data,keys[3])  # grouping on border. getting data indices
+
+    # keys or columns of report.csv
+    reportkeys = ['Border','Date','Measure','Value','Average']
+
+    ## Forming a dictionary to group and sort data based on date, measure and intersection. Calculating Value of a measure in a month on a particular border.
+    f2 = {}
+    for key in reportkeys:
+        f2[key] = []
+
+    for date in sorted(groupby_date,reverse=True):
+        for measure in sorted(groupby_measure):
+            for border in sorted(groupby_border):
+                # intersection gives us the list of indices that are common for a border,measure and date.
+                intersection = list(set(groupby_measure[measure]) & set(groupby_date[date]) & set(groupby_border[border]))
+    #             print(intersection)
+                if len(intersection) != 0:
+    #                 res = [int(data[keys[6]][i]) for i in intersection]   # for small file
+                    res = [int(data[keys[6]][i]) for i in intersection]   # for large file
+    #                 print(border,date,measure,sum(res))
+                    f2[reportkeys[0]].append(border)
+                    f2[reportkeys[1]].append(date)
+                    f2[reportkeys[2]].append(measure)
+                    f2[reportkeys[3]].append(sum(res))
+                    f2[reportkeys[4]].append(0)
+
+    f2 = compute_movavg(f2)
+
+    groupby_value = groupby_column(f2, reportkeys[3])   # grouping on values. getting data indices
+    groupby_average = groupby_column(f2, reportkeys[4])  # grouping on average. getting data indices
+    groupby_date = groupby_column(f2, reportkeys[1])   # grouping on dates. getting data indices
+    groupby_measure = groupby_column(f2, reportkeys[2])  # grouping on measure. getting data indices
+    groupby_border = groupby_column(f2, reportkeys[0])  # grouping on border. getting data indices
+
+    ## Sequentually this is the order in which the sorting of the outut file needs to be. Date -> Value -> Measure -> Border
+    t1 = link_twocols(groupby_date,3) # linking date and value
+    t2 = link_twocols(groupby_value,2) # linking value and measure
+    t3 = link_twocols(groupby_measure,0) # linking measure and border
+
+
+    ## Writing final output to csv
+
+    i = 0
+    j = 0
+    k = 0
+    l = 0
+    # filename = './output/report.csv'
+    filename = outputpath
+    # writing to csv file
+    with open(filename, 'w') as csvfile:
+        # creating a csv writer object
+        csvwriter = csv.writer(csvfile)
+        # writing the fields
+        csvwriter.writerow(reportkeys)
+        for date in t1:
+        #     print(date)
+            i = 0
+            while(i!=len(t1[date])):
+                value = t1[date][i]
+                i = i + 1
+        #         print(value)
+                j = 0
+                while(j!=len(t2[value])):
+                    measure = t2[value][j]
+                    j = j + 1
+        #             print(measure)
+                    k = 0
+                    while(k!=len(t3[measure])):
+                        border = t3[measure][k]
+                        intersection = list(set(groupby_measure[measure]) & set(groupby_date[date]) & set(groupby_border[border]) & set(groupby_value[value]))
+                        if len(intersection)!=0:
+                            index = intersection[0]
+    #                     print(border,date,measure,value,f2[reportkeys[4]][index])
+                            row = [border,date,measure,value,f2[reportkeys[4]][index]]
+                            csvwriter.writerow(row)
+                        break
